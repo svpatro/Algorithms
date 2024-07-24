cdbb38e6bdc322b8556c52559c982b34f49af7b8
Shamik Patro <patroshamik@gmail.com>
2024-07-24 13:28:25 -0500

    Updated CountingSort

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -1,4 +1,43 @@
+/*
+
+Author: Shamik V. Patro
+
+Counting Sort Algorithm Functions:
+
+findFrequencies: Tracks the occurances of each element in the original array by assigning it to a frequency array.
+addFrequencies: Adds the frequencies of the frequency array in increasing order.
+shiftArray: Takes the frequency array and shifts it one index right.
+arrange: Uses the shifted frequency array to arrange the elements of the original array.
+
+*/
+
 #include <iostream>
+
+
+int* findFrequencies(){
+
+
+}
+
+int* addFrequencies(){
+
+
+
+}
+
+int* shiftArray(){
+
+
+
+
+}
+
+
+int* Arrange(){
+
+
+
+}
 
 int main(){
 
