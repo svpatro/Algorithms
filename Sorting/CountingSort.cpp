351aa59dadd68970fd45c967bac69b3ab4b49095
Shamik Patro <patroshamik@gmail.com>
2024-07-26 18:49:22 -0500

    Completed Counting Sort

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -13,34 +13,113 @@ arrange: Uses the shifted frequency array to arrange the elements of the origina
 
 #include <iostream>
 
-
-int* findFrequencies(){
-
-
+int* findFrequencies(int* arr, int size, int* freq){
+    for (int i = 0; i < size; ++i){
+        int count = 0;
+        for (int j = 0; j < size; j++){
+            if (i == arr[j]){
+                count++;
+            }
+        }
+        freq[i] = count;
+    }
+    return freq;
 }
 
-int* addFrequencies(){
-
-
-
+int* addFrequencies(int* arr, int size){
+	for (int i = 1; i < size; i++){
+		arr[i] = arr[i] + arr[i-1];
+	}
+	return arr;
 }
 
-int* shiftArray(){
-
-
-
-
+int* shiftArray(int* arr, int size){
+    for (int i = size-1; i >= 0; i--){
+        arr[i] = arr[i-1];
+    }
+    return arr;
 }
 
+int* arrange(int* freq, int size, int* sorted){
+    for (int i = 0; i < size-1; i++){
+        for (int j = freq[i]; j < freq[i+1]; j++){
+            sorted[j] = i;
+        }
+    }
 
-int* Arrange(){
-
-
-
+    return sorted;
 }
 
 int main(){
 
+    int size = 15;
+    int* arr = new int[size]{1, 6, 3, 7, 8, 2, 9, 5, 4, 0, 7, 7, 7, 7, 5};
+    int* freq = new int[size];
+    int* sorted = new int[size];
+
+    std::cout << "This is the original array:" << std::endl;
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++){
+	    std::cout << arr[i] << " "; 
+    }
+    std::cout << "]";
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "These are the frequencies of each number:" << std::endl;
+
+    freq = findFrequencies(arr, size, freq);
+
+    std::cout << "[ ";
+    for  (int i = 0; i < size; i++){
+	    std::cout << i << " ";
+    }
+    std::cout << "]";
+    std::cout << std::endl;
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++){
+        std::cout << freq[i] << " ";
+    }
+    std::cout << "]";
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "These are the new indices of each number:" << std::endl;
+
+    freq = addFrequencies(freq, size);
+    freq = shiftArray(freq, size);
+
+    std::cout << "[ ";
+
+    for (int i = 0; i < size; i++){
+	    std::cout << i << " ";
+    }
+
+    std::cout << "]";
+    std::cout << std::endl;
+    std::cout << "[ ";
+
+    for (int i = 0; i < size; i++){
+	    std::cout << freq[i] << " ";
+    }
+
+    std::cout << "]";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Here is the sorted array:" << std::endl;
+
+    sorted = arrange(freq, size, sorted);
+
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++){
+        std::cout << sorted[i] << " ";
+    }
+    std::cout << "]";
+    std::cout << std::endl;
+
+    delete [] arr;
+    delete [] freq;
+    delete [] sorted;
 
 return 0;
 
---
+++ Sorting/RadixSort.cpp
@@ -0,0 +1,14 @@
+/*
+ 
+Author: Shamik V. Patro
+
+Radix Sort Algorithm Functions:
+ 
+*/
+
+#include <iostream>
+
+int main(){
+
+	return 0;
+}
